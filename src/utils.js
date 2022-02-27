a89d2c697efdd7d1941219c48077a2a6756061ce
CXPhoenix <jate9229@gmail.com>
2022-02-27 16:22:54 +0800

    production url

--- src/utils.js
+++ src/utils.js
@@ -7,5 +7,5 @@ export function getImage(imageName) {
   return new URL(`./assets/${imageName}`, import.meta.url).href;
 }
 
-// export const url = new URL("/api/", "https://star.fhsh.tp.edu.tw").href;
-export const url = new URL("http://localhost:8000").href;
+export const url = new URL("/api/", "https://star.fhsh.tp.edu.tw").href;
+// export const url = new URL("http://localhost:8000").href;
