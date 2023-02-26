9626f359b3ad69f153c9d7f69b1f793fcbfa5ea4
CXPhoenix <jate9229@gmail.com>
2023-02-26 20:05:49 +0800

    feat: 🎸 切版基本上完成，差一個下一頁的按鈕

--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,9 +1,46 @@
 <script setup>
+import VerticalButton from "../components/VerticalButton.vue";
+
 const getUserInfo = () => {
-  return {};
+  return [
+    [
+      "報名序號",
+      "姓名",
+      "校百分比",
+      "國文級分",
+      "英文級分",
+      "數Ａ級分",
+      "數Ｂ級分",
+      "社會級分",
+      "自然級分",
+      "推薦順序",
+    ],
+    ["0", "1", "2", "2", "2", "2", "2", "2", "2", "2"],
+  ];
 };
 </script>
 
 <template>
-  <div></div>
+  <div
+    class="relative mx-auto flex w-full max-w-xl flex-col items-center justify-center gap-4 py-10 pt-14"
+  >
+    <div
+      class="grid w-full grid-cols-2 justify-items-stretch gap-8 text-xl"
+      v-for="(userInfoKey, index) in getUserInfo()[0]"
+      :key="index.toString().padStart(2, '0') + userInfoKey"
+    >
+      <p class="justify-self-end">{{ userInfoKey }}:</p>
+      <p class="justify-self-start">{{ getUserInfo()[1][index] }}</p>
+    </div>
+    <div class="absolute top-0 right-0">
+      <router-link to="/menu">
+        <VerticalButton bg-color="#fff">
+          <p class="">新增/修改預選志願學群</p>
+          <span class="">
+            <font-awesome-icon icon="fa-solid fa-arrow-right" />
+          </span>
+        </VerticalButton>
+      </router-link>
+    </div>
+  </div>
 </template>
